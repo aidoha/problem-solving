3f404bb3ebead9425084a63eb588a70dac531780
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2021-07-01 23:04:31 +0600

    robot turn to origin

---
+++ loops/robot-turn-to-origin.js
@@ -0,0 +1,29 @@
+const judgeCircle = function (moves) {
+    let xAxis = [0, 0, -1, +1];
+    let yAxis = [+1, -1, 0, 0];
+    let x = 0, y = 0;
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i] === 'R') {
+            x += xAxis[0];
+            y += yAxis[0];
+        }
+        if (moves[i] === 'L') {
+            x += xAxis[1];
+            y += yAxis[1];
+        }
+        if (moves[i] === 'R') {
+            x += xAxis[2];
+            y += yAxis[2];
+        }
+        if (moves[i] === 'R') {
+            x += xAxis[3];
+            y += yAxis[3];
+        }
+    }
+    return x === 0 && y === 0;
+};
+
+judgeCircle("UD")
+// RRDD - false
+// LLUUD - false
+// UD - true
