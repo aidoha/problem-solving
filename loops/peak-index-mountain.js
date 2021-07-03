f7e19eaca6b3fa9996abaca18f3ae7b740a4d376
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2021-07-03 16:04:13 +0600

    Update peak-index-mountain.js

--- loops/peak-index-mountain.js
+++ loops/peak-index-mountain.js
@@ -1,4 +1,4 @@
-const peakIndexInMountainArray = function (arr) {
+const peakIndexInMountainArray = (arr) => {
     let index = 0;
     for (let i = 0; i < arr.length; i++) {
         if (!(arr[i] < arr[i + 1])) {
